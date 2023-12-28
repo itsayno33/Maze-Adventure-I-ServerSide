39554ee3d7a8f5902f6829c194ae046ce30f64b4
itsayno33 <itsayno33@gmail.com>
2023-12-29 00:02:15 +0900

    iphone(縦)のCSSを修正。うまくいかない理由はViewportの設定忘れだった。。。設定後にpx指定の部分をほぼ再設定する羽目になったが、そこそこ満足した。あとは画面ダブルクリックで勝手に拡大しないようにできるかどうか。。。

--- mai_maze/index.php
+++ mai_maze/index.php
@@ -228,6 +228,7 @@ function new_team(): Team {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0 ,user-scalable=0, shrink-to-fit=no, viewport-fit=cover">
     <title>Maze Adventure I</title>
     <link rel="stylesheet" href="css.php?time=<?php echo date("Y-m-d_H:i:s"); ?>&file=mai_index" />
     <script src="./js/bundle.js?time=<?php echo date("Y-m-d_H:i:s"); ?>"></script>
